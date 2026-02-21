834d61cad9f6c7182af25c450b57af2a442c337f
Jay Bagchi <joy.bagchi@gmail.com>
2026-02-20 21:06:11 -0800

    Add webpage fetching tool and create agent for content retrieval

--- agentic_rag/agentic_rag.py
+++ agentic_rag/agentic_rag.py
@@ -20,7 +20,6 @@ import bm25s
 # ---------- Setup (one-time) ----------
 # 1) Build the vector store
 
-os.environ["USER_AGENT"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
 urls = [
     "https://lilianweng.github.io/posts/2024-11-28-reward-hacking/",
     "https://lilianweng.github.io/posts/2024-07-07-hallucination/",
@@ -64,7 +63,8 @@ retriever_tool = create_retriever_tool.invoke(
 
 
 def generate_query_or_respond(state: MessagesState):
-    """Call the model to generate a response based on the current state. Given
+    """
+    Call the model to generate a response based on the current state. Given
     the question, it will decide to retrieve using the retriever tool, or simply respond to the user.
     """
     resp = response_model.bind_tools([retriever_tool]).invoke(state["messages"])
